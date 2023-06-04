4e3e1b6396f754f7ab3aa5af864a30c21acdcd20
christian gallardo <chris.gallardo.guerra@gmail.com>
2023-06-04 17:48:09 -0300

    creo ejecutable

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name="CompraCoder",
+    version="1.0",
+    description="Realiza un login, muestra una lista de productos a comprar y los agrega a tu carrito",
+    author="Christian Gallardo",
+    author_email="chris.gallardo.guerra@gmail.com",
+    packages=["paquete"],
+)
